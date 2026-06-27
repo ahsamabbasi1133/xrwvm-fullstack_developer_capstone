2aa1a9af8a4f85ae8b7082b27e5cbe7ec3b32a42
Ahsam Abbasi <ahsamabbasi1133@gmail.com>
2026-06-27 18:29:59 -0400

    Lab 4: Added CarMake and CarModel Django models

---
+++ server/cardealer/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('MINIVAN', 'Minivan'),
+        ('PICKUP', 'Pickup'),
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2023)
+
+    def __str__(self):
+        return self.name
