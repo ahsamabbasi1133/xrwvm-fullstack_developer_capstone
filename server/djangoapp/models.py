b72c8f5c6db7a9929f7fcb4f2824f5deb9e91d8a
Ahsam Abbasi <YOUR_GITHUB_EMAIL>
2026-09-13 12:21:42 -0400

    Build CarMake and CarModel Django models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -23,3 +23,48 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.name
+
+
+class CarModel(models.Model):
+
+    SEDAN = "Sedan"
+    SUV = "SUV"
+    WAGON = "Wagon"
+
+    CAR_TYPES = [
+        (SEDAN, "Sedan"),
+        (SUV, "SUV"),
+        (WAGON, "Wagon"),
+    ]
+
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE,
+        related_name="models"
+    )
+
+    dealer_id = models.IntegerField(default=0)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=CAR_TYPES, default=SEDAN)
+    year = models.IntegerField(
+        validators=[
+            MinValueValidator(2015),
+            MaxValueValidator(2023)
+        ]
+    )
+    created_at = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name}"
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -9,6 +9,8 @@ import json
 from django.contrib.auth import login, authenticate
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from .models import CarMake, CarModel
+from .populate import initiate
 
 logger = logging.getLogger(__name__)
 
@@ -52,3 +54,20 @@ def registration(request):
     else:
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+
+    if count == 0:
+        initiate()
+
+    car_models = CarModel.objects.select_related('car_make')
+
+    cars = []
+    for car_model in car_models:
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
+
+    return JsonResponse({"CarModels": cars})
